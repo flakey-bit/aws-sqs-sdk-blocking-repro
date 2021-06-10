5a1088f7d833adfc7d6e3a1592d00775ab6f4ac0
eddie.stanley <eddie.stanley@xero.com>
2021-06-11 08:32:33 +1200

    Initial commit showing client/server interaction over HTTP where client receives headers and then waits indefinitely for content

---
+++ client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string url = "http://localhost:5000/";
+            var client = new HttpClient()
+            {
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url)
+            };
+
+            var sw = Stopwatch.StartNew();
+            var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            Console.Out.WriteLine($"{sw.Elapsed} Got headers: \n{response.Headers}");
+
+            var body = await response.Content.ReadAsStringAsync();
+            Console.Out.WriteLine($"{sw.Elapsed} Got body: \n{body}");
+        }
+    }
+}
---
+++ server/Controllers/SQSController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class SQSController : ControllerBase
+    {
+        public SQSController(ILogger<SQSController> logger)
+        {
+        }
+
+        [HttpPost]
+        public async Task<EmptyResult> Post()
+        {
+            Response.ContentType = "text/xml";
+            string xml = "<ReceiveMessageResponse>\n  <ReceiveMessageResult />\n  <ResponseMetadata>\n    <RequestId>b6633655-283d-45b4-aee4-4e84e0ae6afa</RequestId>\n  </ResponseMetadata>\n</ReceiveMessageResponse>";
+            var bytes = Encoding.UTF8.GetBytes(xml);
+
+            Response.Headers.ContentLength = bytes.Length;
+            await Response.Body.FlushAsync();
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await new MemoryStream(bytes).CopyToAsync(Response.Body);
+            return new EmptyResult();
+        }
+    }
+}
