a4894fa1dae0c3f799f35beeb2bfacf9d18bd174
eddie.stanley <eddie.stanley@xero.com>
2021-08-25 16:13:46 +1200

    Showed attempt to address the issue cooperatively (via a cancellation token). This doesn't work (unless the CT is already cancelled prior to entering)

--- client/Program.cs
+++ client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Amazon.SQS;
 using Amazon.SQS.Model;
@@ -38,9 +39,27 @@ namespace client
             };
 
             var sw = Stopwatch.StartNew();
-            var response = await client.ReceiveMessageAsync(request);
 
-            Console.Out.WriteLine($"{sw.Elapsed} Got messages: \n{response.Messages.Count}");
+            //var response = await client.ReceiveMessageAsync(request);
+
+            var getSqsTimeout = CancellationTokenSource.CreateLinkedTokenSource(new CancellationToken());
+            getSqsTimeout.CancelAfter(TimeSpan.FromSeconds(5));
+
+            try
+            {
+                var response = await client.ReceiveMessageAsync(request, getSqsTimeout.Token).ConfigureAwait(false);
+                Console.Out.WriteLine($"{sw.Elapsed} Got messages: \n{response.Messages.Count}");
+            }
+            catch (TaskCanceledException exception) when (getSqsTimeout.IsCancellationRequested)
+            {
+                Console.Out.WriteLine("GetSqsMessages canceled.");
+            }
+            catch (OperationCanceledException)
+            {
+                Console.Out.WriteLine("GetSqsMessages canceled.");
+            }
+
+
         }
     }
 }
